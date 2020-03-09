08102d7ac7f9d5c950c9b55c26dea4d47794be3c
tetunori <tetunori@gmail.com>
2020-03-09 20:46:27 +0900

    Implemented DivSquare

---
+++ Ch1_Euclid/DivSquare/sketch.js
@@ -0,0 +1,62 @@
+
+const WIDTH = 500;
+
+function setup() {
+  createCanvas( WIDTH, WIDTH );
+}
+
+function draw() {
+
+  const numA  = 17;
+  const numB =  29;
+  const RATIO = numB / numA;
+
+  let squareWidth = WIDTH;
+  let xPos = 0;
+  let yPos = 0;
+  let itr = 0;
+
+  colorMode( HSB, 100 );
+
+  while( squareWidth > 0 ){
+
+    itr++;
+    if( !isEven( itr ) ){
+
+      // If iteration number is odd, add square in x-axis direction.
+      while( xPos + squareWidth * RATIO <= WIDTH + 0.1 ){
+        
+        const col = color( random( 100 ), 100, 100 );
+        fill( col );
+        rect( xPos, yPos, squareWidth * RATIO, squareWidth );
+        xPos += squareWidth * RATIO;
+
+      }
+      squareWidth = WIDTH - xPos;
+
+    }else{
+
+      // If iteration number is even, add square in y-axis direction.
+      while ( yPos + squareWidth / RATIO <= WIDTH + 0.1 ){
+
+        const col = color( random( 100 ), 100, 100 );
+        fill( col );
+        rect( xPos, yPos, squareWidth, squareWidth / RATIO );
+        yPos += squareWidth / RATIO;
+        
+      }
+      squareWidth = WIDTH - yPos;
+
+    }
+
+  }
+
+  noLoop();
+
+}
+
+// The number is even or not.
+const isEven = ( number ) => {
+  return ( number % 2 === 0 );
+}
+
