1d00892c7824256a250c0531bd4cf2ed76a72f6d
tetunori <tetunori@gmail.com>
2020-03-09 23:02:32 +0900

    Implemented DivSquareRecorder

---
+++ Ch1_Euclid/DivSquareRecorder/sketch.js
@@ -0,0 +1,64 @@
+
+const WIDTH = 500;
+
+function setup() {
+  createCanvas( WIDTH, WIDTH );
+}
+
+function draw() {
+
+  const numA = 10;
+  const numB =  6;
+  const RATIO = numB / numA;
+
+  let squareWidth = WIDTH;
+  let xPos = 0;
+  let yPos = 0;
+  let itr = 0;
+
+  colorMode( HSB, 100 );
+  const namePNG = '' + numA + '_' + numB + '.png';
+
+  while( squareWidth > 0 ){
+
+    itr++;
+    if( !isEven( itr ) ){
+
+      // If iteration number is odd, add square in x-axis direction.
+      while( xPos + squareWidth * RATIO <= WIDTH + 0.1 ){
+        
+        const col = color( random( 100 ), 100, 100 );
+        fill( col );
+        rect( xPos, yPos, squareWidth * RATIO, squareWidth );
+        xPos += squareWidth * RATIO;
+
+      }
+      squareWidth = WIDTH - xPos;
+
+    }else{
+
+      // If iteration number is even, add square in y-axis direction.
+      while ( yPos + squareWidth / RATIO <= WIDTH + 0.1 ){
+
+        const col = color( random( 100 ), 100, 100 );
+        fill( col );
+        rect( xPos, yPos, squareWidth, squareWidth / RATIO );
+        yPos += squareWidth / RATIO;
+        
+      }
+      squareWidth = WIDTH - yPos;
+
+    }
+
+  }
+
+  noLoop();
+  saveCanvas( namePNG, 'png' );
+
+}
+
+// The number is even or not.
+const isEven = ( number ) => {
+  return ( number % 2 === 0 );
+}
+
