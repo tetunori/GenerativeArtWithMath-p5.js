2b9f1c6e4f8cfd32261788b9f4ec0d6a9e62f4f0
tetunori <tetunori@gmail.com>
2020-03-31 21:49:44 +0900

    Implemented Koch

---
+++ Ch11_Deformation/Koch/Koch.js
@@ -0,0 +1,28 @@
+
+const WIDTH = 500;
+const HEIGHT = 500;
+
+let gVector1;
+let gVector2;
+let gUpperLimit = 0;
+
+function setup() {
+
+  createCanvas( WIDTH, HEIGHT );
+  colorMode( HSB, 100 );
+
+  gVector1 = createVector( 0, HEIGHT / 2 );
+  gVector2 = createVector( WIDTH, HEIGHT / 2 );
+
+  drawCurve();
+
+}
+
+function draw() {}
+
+function mouseClicked() {
+  
+  gUpperLimit++;
+  drawCurve();
+
+}
